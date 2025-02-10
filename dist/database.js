c0bc53ad49185d96f5de1c663df217d5fd96df2b
Deepanshu Sharma <deepanshu2210sharma@gmail.com>
2025-02-10 18:11:32 +0530

    change dist

--- dist/database.js
+++ dist/database.js
@@ -13,7 +13,10 @@ const pool = promise_1.default.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: true,
+    },
 });
 pool.getConnection()
     .then((connection) => {
